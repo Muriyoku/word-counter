d3f702f4c64955d69db644d241a28fb7c25c2a6b
zafkiel45 <zafkielc@gmail.com>
2025-10-29 21:56:13 -0300

    feat: add result on a file

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,26 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::Read;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
 use std::vec;
 
 fn main() {
     let mut word_collection: HashMap<String, i64> = HashMap::new();
 
     word_counter(get_source().unwrap(), &mut word_collection);
-
-    let t = format!("{word_collection:?}");
-
-    println!("{}", t)
+    register_result(&word_collection).unwrap();
 }
 
 fn word_counter(source: Vec<Vec<String>>, list: &mut HashMap<String, i64>) {
     for l in source {
         for w in l {
-            list.entry(w)
-            .and_modify(| c| *c += 1)
-            .or_insert(1);
+            list.entry(w).and_modify(|c| *c += 1).or_insert(1);
         }
     }
 }
 
-fn get_source() -> Result<Vec<Vec<String>>, String>{
-    let text: Result<File, std::io::Error> = File::open("./source.txt"); 
+fn get_source() -> Result<Vec<Vec<String>>, String> {
+    let text: Result<File, std::io::Error> = File::open("./source.txt");
     let mut buffer: String = String::new();
 
     match text {
@@ -37,24 +33,50 @@ fn get_source() -> Result<Vec<Vec<String>>, String>{
                     let mut clear_buffer: String = String::new();
 
                     for l in buffer.lines() {
-                        clear_buffer = l.chars()
-                        .filter(|c| c.is_alphanumeric() || c.is_whitespace()) 
-                        .collect()
-                       
+                        // remove headers from some message apps, like 21/03/25 Doe:
+                        let line = match l.split_once(": ") {
+                            Some((_, text)) => text,
+                            None => l,
+                        };
+
+                        let cleaned: String = line
+                            .chars()
+                            .filter(|c| c.is_alphanumeric() || c.is_whitespace())
+                            .collect();
+
+                        clear_buffer.push_str(&cleaned);
+                        clear_buffer.push('\n');
                     }
 
                     for l in clear_buffer.to_lowercase().lines() {
-                        words_vec.push(
-                            l.split(" ")
-                            .map(|w| w.to_string())
-                            .collect()
-                        );
+                        words_vec.push(l.split(" ").map(|w| w.to_string()).collect());
                     }
 
-                    return Ok(words_vec)
-                },
-                Err(e) => return Err(e.kind().to_string())
+                    return Ok(words_vec);
+                }
+                Err(e) => return Err(e.kind().to_string()),
             }
+        }
+        Err(e) => return Err(e.kind().to_string()),
+    }
+}
+
+fn register_result(list: &HashMap<String, i64>) -> Result<(), String> {
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("./result.txt");
+    let mut buffer = String::new();
+
+    for (k, v) in list {
+        buffer.push_str(format!("repeated: {k} {v} times\n").as_str());
+    }
+
+    match file {
+        Ok(mut f) => match f.write(buffer.as_bytes()) {
+            Ok(_) => return Ok(()),
+            Err(e) => return Err(e.kind().to_string()),
         },
         Err(e) => return Err(e.kind().to_string()),
     }
